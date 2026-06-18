26601adb34784f32fa2d96a5e4a235ae6a70ea19
Vignesh <karthiksound2128@gmail.com>
2026-06-18 11:16:57 +0530

    Create ReportForm class for report generation UI

---
+++ ReportForm.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class ReportForm extends JFrame {
+
+    ReportForm() {
+        setTitle("Reports");
+        setSize(400,300);
+
+        add(new JLabel("Report Generation"));
+
+        setVisible(true);
+    }
+}
