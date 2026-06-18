5bb5b99abb3083aa1f27576a870f663ff94cd991
Vignesh <karthiksound2128@gmail.com>
2026-06-18 11:16:36 +0530

    Create BillingForm.java

---
+++ BillingForm.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class BillingForm extends JFrame {
+
+    BillingForm() {
+        setTitle("Billing System");
+        setSize(400,300);
+
+        add(new JLabel("Billing Details"));
+
+        setVisible(true);
+    }
+}
