a32da7b036100c2911d0555cbb67691ea0bc4ffe
Vignesh <karthiksound2128@gmail.com>
2026-06-18 11:15:42 +0530

    Create SupplierForm class for supplier management UI

---
+++ SupplierForm.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class SupplierForm extends JFrame {
+
+    SupplierForm() {
+        setTitle("Supplier Management");
+        setSize(400,300);
+
+        add(new JLabel("Supplier Details"));
+
+        setVisible(true);
+    }
+}
