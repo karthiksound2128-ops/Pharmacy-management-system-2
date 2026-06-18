0928201b62d94267acb4a2b7dbba283435b24d28
Vignesh <karthiksound2128@gmail.com>
2026-06-18 11:16:05 +0530

    Create CustomerForm class for customer management

---
+++ CustomerForm.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class CustomerForm extends JFrame {
+
+    CustomerForm() {
+        setTitle("Customer Management");
+        setSize(400,300);
+
+        add(new JLabel("Customer Details"));
+
+        setVisible(true);
+    }
+}
